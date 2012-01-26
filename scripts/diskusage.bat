429ee8c16655ddacc762b85917af9dab4e0c951e
Jörn Dinkla <joern@dinkla.net>
2012-01-26 18:34:19 +0100

    0.4

--- scripts/diskusage.bat
+++ scripts/diskusage.bat
@@ -1,2 +1,2 @@
-groovy -Xms512m -Xmx1024m --classpath diskusage-0.4.jar diskusage.groovy
+groovy --classpath diskusage-0.4.jar diskusage.groovy
 
--- scripts/diskusage.sh
+++ scripts/diskusage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-groovy -Xms512m -Xmx1024m --classpath diskusage-0.4.jar diskusage.groovy
+groovy --classpath diskusage-0.4.jar diskusage.groovy
